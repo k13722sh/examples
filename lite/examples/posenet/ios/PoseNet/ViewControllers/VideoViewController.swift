47e3ce4b2f29130c5c6e6b9d26ebbd26a903155a
Sean Harris <seanharris@Seans-MacBook-Pro.local>
2021-03-01 10:32:53 +0000

    Play a prerecorded video stored in the app on the appropriate screen - Test footage stored in the app bundle plays on the correct screen

--- lite/examples/posenet/ios/PoseNet/ViewControllers/VideoViewController.swift
+++ lite/examples/posenet/ios/PoseNet/ViewControllers/VideoViewController.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2021 tensorflow. All rights reserved.
 //
 
+import AVFoundation
+import AVKit
 import UIKit
 
 class VideoViewController: UIViewController {
@@ -15,6 +17,21 @@ class VideoViewController: UIViewController {
 
         view.backgroundColor = .systemOrange
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "RightShoulderAlfie", ofType: "MOV")!))
+        // Object to render player to view
+        let layer = AVPlayerLayer(player: player)
+        // Set layer frame to be the entire view
+        layer.frame = view.bounds
+        // Add as a sublayer to the views layer
+        view.layer.addSublayer(layer)
+        
+        player.play()
+        
     }
     
 
